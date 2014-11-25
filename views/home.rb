ecff1340cd6f33d5f663df30327331e55e4b1ea7
Lyd <Lyd@lydia-mbp.fritz.box>
2014-11-25 21:03:40 +0100

    nicht nötige Methode gelöscht
    def task…if else gelöscht

--- views/home.rb
+++ views/home.rb
@@ -4,13 +4,8 @@ module ToDo
       class Index
         include ToDo::View
 
-     def aufgabenliste
-          if task
-            "{task}!"
-          else
-            "nothing else to do?"
-          end
-        end
+     
+        
 
       end
     end
